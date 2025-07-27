f0ccb234ed0c296d50d00522585be679e352805a
aviralkaushik412 <aviralkaushik412@gmail.com>
2025-07-27 13:25:58 +0530

    27-07-2025/2 - made env file safe

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBfH5a32tg5bJ2WrO-By-xBaS3ttUpRT4o",
-//   authDomain: "skillmirror-237a3.firebaseapp.com",
-//   projectId: "skillmirror-237a3",
-//   storageBucket: "skillmirror-237a3.firebasestorage.app",
-//   messagingSenderId: "787332916090",
-//   appId: "1:787332916090:web:4d42b0da472151fe3decc3",
-//   measurementId: "G-F5CX3ZXBQ3"
-// };
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
